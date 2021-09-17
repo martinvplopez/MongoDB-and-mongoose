4747b2453d55f40b15d25892f9d0c91514356db3
martinvplopez <vanpu3d@gmail.com>
2021-09-17 17:42:14 +0200

    Adding mongodb connection and creating mongoose schema

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
+const { Schema, Number } = require('mongoose');
+let mongoose=require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+let personSchema=new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods:[String]
+}) ;
 
 let Person;
 
